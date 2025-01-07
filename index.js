63e8accbe8df2c330cc2e4ca045d106df6943a85
Sorav-Mishra <abhiishekmishraa2025@gmail.com>
2025-01-07 20:49:10 +0530

    aa

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 let teaData = [];
 let nextId = 1;
 
+// create a tea order
 app.post("/teas", (req, res) => {
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
@@ -16,9 +17,40 @@ app.post("/teas", (req, res) => {
   res.send(newTea).status(201);
 });
 
+// get all the teas
 app.get("/teas", (req, res) => {
   res.send(teaData);
   res.status(200);
+});
+
+// get teas with id
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    res.status(404).send("tea not found");
+  }
+  res.status(200).send(tea);
+});
+
+// updated tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    res.status(404).send("Not found");
+  }
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+});
+
+// detele tea
+app.delete("delete/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Not found");
+  }
+  teaData.splice(index, 1);
+  return res.status(205).send(teaData);
 });
 
 app.listen(port, () => {
